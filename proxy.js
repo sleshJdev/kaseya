f22053477a756cfa32e72278524612a9dbdef130
sleshJdev <slesh.eugene93@gmail.com>
2021-03-11 19:10:11 +0300

    Formating

--- proxy.js
+++ proxy.js
@@ -4,13 +4,13 @@ const config = require('./config');
 const authorizeRequest = require('./authorize-request');
 
 http.createServer(async (req, res) => {
-    console.log(`Request ${req.method} ${req.url}, headers: ${req.rawHeaders}`)
+    console.log(`Request ${req.method} ${req.url}, headers: ${req.rawHeaders}`);
     const kaseyaRes = await authorizeRequest({
         method: req.method,
         path: req.url,
         headers: req.headers,
     });
-    res.setHeader('Content-Type', 'application/json; charset=utf-8')
+    res.setHeader('Content-Type', 'application/json; charset=utf-8');
     res.write(JSON.stringify(kaseyaRes));
     res.end();
 }).listen(config.PROXY_PORT);
