49a66f8bd99688ce41623d4419faf6f3ae6e05ea
sleshJdev <slesh.eugene93@gmail.com>
2021-03-11 22:01:07 +0300

    Support jsonl response format

--- proxy.js
+++ proxy.js
@@ -1,9 +1,12 @@
 const http = require('http');
+const { URL } = require('url');
 const config = require('./config');
 
 const authorizeRequest = require('./authorize-request');
 
-http.createServer(async (req, res) => {    
+http.createServer(async (req, res) => {
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const accept = (url.searchParams['accept'] || 'json').toLocaleLowerCase().trim();
     const reqAttrs = {
         method: req.method,
         path: req.url,
@@ -11,7 +14,11 @@ http.createServer(async (req, res) => {
     };
     console.log(`Request ${JSON.stringify(reqAttrs)}`);
     const kaseyaRes = await authorizeRequest(reqAttrs);
-    res.setHeader('Content-Type', 'application/json; charset=utf-8');
-    res.write(JSON.stringify(kaseyaRes));
+    res.setHeader('Content-Type', `application/${accept}; charset=utf-8`);
+    if (accept == 'jsonl') {
+        res.write(kaseyaRes.map(it => JSON.stringify(it)).join('\n'));
+    } else {
+        res.write(JSON.stringify(kaseyaRes));
+    }
     res.end();
 }).listen(config.PROXY_PORT);
