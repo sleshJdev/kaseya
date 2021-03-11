569ac61a0f3bdc86ef0c878a1316b6b4f7d849c6
sleshJdev <slesh.eugene93@gmail.com>
2021-03-11 19:18:35 +0300

    Imrove logging

--- proxy.js
+++ proxy.js
@@ -3,13 +3,14 @@ const config = require('./config');
 
 const authorizeRequest = require('./authorize-request');
 
-http.createServer(async (req, res) => {
-    console.log(`Request ${req.method} ${req.url}, headers: ${req.rawHeaders}`);
-    const kaseyaRes = await authorizeRequest({
+http.createServer(async (req, res) => {    
+    const reqAttrs = {
         method: req.method,
         path: req.url,
         headers: req.headers,
-    });
+    };
+    console.log(`Request ${JSON.stringify(reqAttrs)}`);
+    const kaseyaRes = await authorizeRequest(reqAttrs);
     res.setHeader('Content-Type', 'application/json; charset=utf-8');
     res.write(JSON.stringify(kaseyaRes));
     res.end();
